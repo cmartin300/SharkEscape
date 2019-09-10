a5cb8efebca51eed0e05ea35eab300e4f2a2a73e
Chris <13564494+cmartin300@users.noreply.github.com>
2019-09-10 18:20:04 -0400

    Mechanics Update
    Movement Mechanic
    Camera Scroll
    Die if you reach top of screen
    character animations

---
+++ Shark Escape/Assets/CamScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScroller : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private float moveSpeed;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        var camPosition = cam.transform.position;
+        camPosition.y -= moveSpeed * Time.deltaTime;
+        cam.transform.position = camPosition;
+    }
+}
---
+++ Shark Escape/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float minXPosition;
+    [SerializeField] private float maxXPosition;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+
+        if (direction < 0)
+            anim.SetBool("SwimmingRight", false);
+        if (direction > 0)
+            anim.SetBool("SwimmingRight", true);
+
+        if (Mathf.Abs(direction) > 0)
+        {
+            if (transform.position.x >= minXPosition && transform.position.x <= maxXPosition)
+                transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+            else if (transform.position.x > maxXPosition)
+                transform.position = new Vector2(maxXPosition, transform.position.y);
+            else if (transform.position.x < minXPosition)
+                transform.position = new Vector2(minXPosition, transform.position.y);
+        }
+    }
+}
